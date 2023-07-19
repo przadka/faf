2a4388d10231a6b37f560de4ffa37642678c2942
Michał Prządka <przadka@gmail.com>
2023-07-19 10:37:27 +0200

    Prompt improvements

--- src/faf/main.py
+++ src/faf/main.py
@@ -6,20 +6,23 @@ from langchain.chat_models import ChatOpenAI
 from langchain import  LLMChain
 
 from helpers import get_env_var, validate_user_input
-from tools import follow_up_then, self_note, save_url
+from tools import follow_up_then, user_note, save_url
 
 # Define prefix and suffix for creating prompts
 prefix = """Answer the following questions as best you can.
 Note that the user will sometimes talk as if they were giving instructions to you, but in fact they want you 
-to send these instructions to them, either as reminders or follow ups. 
+to send these instructions to them, either as reminders or follow ups. Never replace user input with URLs or
+other links. Always use the tools provided below to process the input.
 You have access to the following tools, but you are only allowed to use one of them:"""
-suffix = """Begin! Remember - you only need to use one tool. 
+suffix = """Begin! Remember - you only need to use one tool and always act with a tool in your first action.
+If you don't know what to do, pass the user note to Self Note tool, without any modification.
+Never ask for clarification.
 
-Question: {input}
-{agent_scratchpad}"""
+User wants to you to process the following input using only the tools above, in a single step. Choose the best tool for it: {input}
+{agent_scratchpad} """
 
 fut = StructuredTool.from_function(follow_up_then, name="Follow Up Then")
-note = StructuredTool.from_function(self_note, name="Note to Self")
+note = StructuredTool.from_function(user_note, name="Note to Self")
 url = StructuredTool.from_function(save_url, name="Save URL")
 
 tools = [note, fut, url]
@@ -44,7 +47,6 @@ if __name__ == "__main__":
     try:
         # Check that the necessary environment variables are set
         openai_key = get_env_var('OPENAI_API_KEY')
-        ifttt_key = get_env_var('IFTTT_KEY')
         
         msg = sys.argv[1]
         validate_user_input(msg)
