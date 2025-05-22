c36b2c8ad5fb411116369dd7fe2e2fc45ec38898
Michal Przadka <przadka@gmail.com>
2025-05-22 07:19:52 +0200

    Add test session to noxfile and fix imports

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,15 @@
 import nox
 from nox.sessions import Session
 
+@nox.session(name="test")
+@nox.session(name="tests")
+def run_tests(session: Session) -> None:
+    """Run the test suite with pytest."""
+    session.install('-r', 'requirements.txt')
+    session.install('pytest-cov')  # Install pytest-cov for coverage reports
+    # Use python -m pytest to include the current directory in the Python path
+    session.run("python", "-m", "pytest", "--cov=src.faf")
+
 @nox.session
 def package(session: Session) -> None:
     session.install('-r', 'requirements.txt')
